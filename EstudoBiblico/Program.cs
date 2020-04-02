ce0a2acc5bd4781d37a437440d9b2d7b9f76e9df
Luisa Salles <luisasalles@outlook.com>
2020-04-02 10:41:29 -0300

    Adicionar linha

--- EstudoBiblico/Program.cs
+++ EstudoBiblico/Program.cs
@@ -7,6 +7,7 @@ namespace EstudoBiblico
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Testando ...");
         }
     }
 }
